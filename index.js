bf7b7015ad499356c00b9699f304e868ecfb6f1c
Junior Albuquerque <junioralbuquerque81@gmail.com>
2022-07-10 19:34:32 -0400

    fix: vercel config

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-
-// Minimal puppeteer get page HTML source code example
-const puppeteer = require('puppeteer');
-const cherio = require('cheerio');
-
-(async () => {
-  const parseHtml = (html) => {
-    const metaTags = []
-    const htmlContent = cherio.load(html)
-    htmlContent('head meta').map((i, item) => {
-      if (!item.attribs) return null
-      if (!item.attribs.property) return null
-      if (!item.attribs.content) return null
-
-      const property = item.attribs.property
-      const content = item.attribs.content
-
-      metaTags.push({
-        tag: property,
-        value: content
-      })
-    })
-
-    console.log(metaTags)
-  }
-
-
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://jnr.vercel.app/', {waitUntil: 'domcontentloaded'});
-  // Wait for 5 seconds
-  // console.log(await page.content());
-  const res = await page.content()
-  parseHtml(res)
-  // Take screenshot
-  await browser.close();
-})();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ScrapController_1 = require("./dist/controllers/ScrapController");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const scrapController = new ScrapController_1.ScrapController();
+class Server {
+    constructor(srapController) {
+        this.srapController = srapController;
+        this.port = 3333;
+        this.baseUrl = '/api/v1';
+        this.app = (0, express_1.default)();
+        this.config();
+    }
+    config() {
+        this.app.set('port', this.port);
+        this.app.use((0, cors_1.default)());
+        this.app.use(express_1.default.json());
+        this.routes();
+    }
+    routes() {
+        this.app.use(this.baseUrl, this.srapController.router);
+        this.app.get('/', (req, res) => {
+            res.json({ message: 'Scrap api :D' });
+        });
+    }
+    start() {
+        this.app.listen(this.app.get('port') || 3333, () => {
+            console.log(`Server running ir port ${this.app.get('port')}`);
+        });
+    }
+}
+const server = new Server(scrapController);
+server.start();
